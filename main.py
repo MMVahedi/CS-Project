34a965235ef52cbb16d0d73a561ba67c3baf2437
Mohammad Mahdi Vahedi <m.m.vahedi13800@gmail.com>
2022-07-24 20:11:44 +0430

    Add Queue to services

--- main.py
+++ main.py
@@ -10,29 +10,57 @@ class Service:
     customers_management_resources = None
     delivery_communication_resources = None
     restaurant_management_resources = None
+    mobile_portal_queue = []
+    order_management_queue = []
+    payment_queue = []
+    web_portal_queue = []
+    customers_management_queue = []
+    delivery_communication_queue = []
+    restaurant_management_queue = []
+    mobile_portal_queue_time_sum = 0
+    order_management_queue_time_sum = 0
+    payment_queue_time_sum = 0
+    web_portal_queue_time_sum = 0
+    customers_management_queue_time_sum = 0
+    delivery_communication_queue_time_sum = 0
+    restaurant_management_queue_time_sum = 0
 
     def __init__(self, type):
         self.type = type
         if self.type == "mobile portal service":
             self.resources = Service.mobile_portal_resources
+            self.queue = Service.mobile_portal_queue
+            self.queue_time_sum = Service.mobile_portal_queue_time_sum
             self.mean = 1.0 / 2
         elif self.type == "order management service":
             self.resources = Service.order_management_resources
+            self.queue = Service.order_management_queue
+            self.queue_time_sum = Service.order_management_queue_time_sum
             self.mean = 1.0 / 6
         elif self.type == "payment service":
             self.resources = Service.payment_resources
+            self.queue = Service.payment_queue
+            self.queue_time_sum = Service.payment_queue_time_sum
             self.mean = 1.0 / 12
         elif self.type == "web portal service":
             self.resources = Service.web_portal_resources
+            self.queue = Service.web_portal_queue
+            self.queue_time_sum = Service.web_portal_queue_time_sum
             self.mean = 1.0 / 3
         elif self.type == "customers management service":
             self.resources = Service.customers_management_resources
+            self.queue = Service.customers_management_queue
+            self.queue_time_sum = Service.customers_management_queue_time_sum
             self.mean = 1.0 / 5
         elif self.type == "delivery communication":
             self.resources = Service.delivery_communication_resources
+            self.queue = Service.delivery_communication_queue
+            self.queue_time_sum = Service.delivery_communication_queue_time_sum
             self.mean = 1.0 / 9
         elif self.type == "restaurant management service":
             self.resources = Service.restaurant_management_resources
+            self.queue = Service.restaurant_management_queue
+            self.queue_time_sum = Service.restaurant_management_queue_time_sum
             self.mean = 1.0 / 8
 
 
@@ -115,13 +143,16 @@ def request_generator(rate):
 def general_service(request):
     global env
     service = request.current_process()
+    service.queue.append(request.id)
     time_entered_queue = env.now
     print(service.type, "time_entered_queue: ", time_entered_queue, request.id)
     with service.resources.request() as req:
         yield req
         time_lef_queue = env.now
+        service.queue.pop(0)
         print(service.type, "time_lef_queue: ", time_lef_queue, request.id)
         time_in_queue = time_lef_queue - time_entered_queue
+        service.queue_time_sum += time_in_queue
         if len(request.processes) > request.turn + 1:
             request.next_process()
             yield env.process(general_service(request))
@@ -131,6 +162,7 @@ def general_service(request):
         yield env.timeout(service_time)
 
 
+Queues_length = {}
 env = simpy.Environment()
 
 number_of_resources = list(map(int, input().split()))
